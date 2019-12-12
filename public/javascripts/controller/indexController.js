5248bc08565fd3fc6c4d237b89c1d5954634abcd
Ozgur <b.ozgurgumus@gmail.com>
2019-12-12 12:56:19 -0500

    newUser function in socket

--- public/javascripts/controller/indexController.js
+++ public/javascripts/controller/indexController.js
@@ -1,13 +1,27 @@
 app.controller('indexController', ['$scope', 'indexFactory', ($scope, indexFactory) =>{
     
-    const connectionOptions = {
-        reconnectionAttempts: 3,
-        reconnectionDelay: 600
+    $scope.init = () =>{
+        const username = prompt('please Enter username');
+        if(username){
+            initSocket(username);
+        }
+        else{
+            return false;
+        }
+    };
+
+    function initSocket(username){
+        const connectionOptions = {
+            reconnectionAttempts: 3,
+            reconnectionDelay: 600
+        }
+        indexFactory.connectSocket('http://localhost:3000', connectionOptions)
+            .then((socket)=>{
+                socket.emit('newUser', {username});
+                console.log('baglanti gerceklesti->> ', socket);
+            }).catch((err)=>{
+                console.log(err);
+            });
     }
-    indexFactory.connectSocket('http://localhost:3000', connectionOptions)
-        .then((socket)=>{
-            console.log('baglanti gerceklesti->> ', socket);
-        }).catch((err)=>{
-            console.log(err);
-        });
+    
 }]);
